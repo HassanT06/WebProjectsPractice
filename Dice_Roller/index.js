476258acf5ece55cb2ad4f6fac846f0c9a7de128
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-03-24 09:50:26 +0000

    Implemented Choice for Number Of Sides for Dice and appended styles (good independent individual progress )

--- Dice_Roller/index.js
+++ Dice_Roller/index.js
@@ -2,11 +2,12 @@ function rolldice() {
     const numOfDice = document.getElementById("NumOfDice").value;
     const DiceResult = document.getElementById("DiceResult");
     const ImagesResult = document.getElementById("ImagesResult");
+    const numOfSides = document.getElementById("NumOfSides").value;
     let values = [];
     let images = [];
 
     for (let i = 0; i < numOfDice; i++) {
-        let value = Math.floor(Math.random() * 6) + 1;
+        let value = Math.floor(Math.random() * numOfSides) + 1;
         values.push(value);
         images.push(`<img src="Images/Pin${value}.png" alt="Dice ${value}">`);
         console.log(images);
