149d816253ec52f3a036a63143517149e5e97513
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-03-31 13:44:22 +0100

    Implemented colour square addition feature

--- Colour Palette/script.js
+++ Colour Palette/script.js
@@ -1,26 +1,39 @@
-newColour = document.getElementById("new");
+let newColour = document.getElementById("new");
+let Container = document.getElementById("Container");
 let colour;
 let colourtype ="";
 newColour.addEventListener("click", event => {
     colour = prompt("Enter a colour value: (rgb=255,0,0) (hex=#ff0000) (hsl=0,100%,50%)");
+    let colourBlock = document.createElement("div");
+    colourBlock.className = "colours";
+    let children = Container.children;
+    if (children.length > 0) {
+        Container.insertBefore(colourBlock,children[children.length-1]);
+    }
+    else {
+        Container.appendChild(colourBlock);
+    }
     if (colour != null && colour != "") {
         // Check colour
         switch (true){
             case (colour.includes("%") && colour.includes(",")):
                 console.log("hsl colour value detected.");
+                colourBlock.style.backgroundColor = `hsl(${colour})`;
                 break;
             case (colour.includes(",")):
                 console.log("rgb colour value detected.");
+                colourBlock.style.backgroundColor = `rgb(${colour})`;
                 break;
             case (colour.includes("#")):
                 console.log("hex colour value detected.");
+                colourBlock.style.backgroundColor = `${colour}`;
                 break;
             default:
                 alert("Please enter a valid colour value.");
                 return;
         }
-
-        document.body.style.backgroundColor = colour;
+        console.log(colour);
+        
         console.log(document.body.style.backgroundColor)
     }
     else {
