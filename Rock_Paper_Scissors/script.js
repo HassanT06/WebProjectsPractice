45e893841324a94727b6b3861108d1941f394564
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-04-02 11:57:53 +0100

    Add ids to dynamicaly change elements

---
+++ Rock_Paper_Scissors/script.js
@@ -0,0 +1,8 @@
+rock = document.getElementById('rock');
+paper = document.getElementById('paper');
+scissors = document.getElementById('scissors');
+result = document.getElementById('result');
+playerScore = document.getElementById('pScore');
+computerScore = document.getElementById('cScore');
+playerChoice = document.getElementById('playerChoice');
+computerChoice = document.getElementById('computerChoice');
