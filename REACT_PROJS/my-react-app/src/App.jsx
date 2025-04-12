9caf685fdd2acaf174e971573e49963d98357ad6
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-04-12 21:34:57 +0100

    Basic Counter via REACT

--- REACT_PROJS/my-react-app/src/App.jsx
+++ REACT_PROJS/my-react-app/src/App.jsx
@@ -2,18 +2,21 @@
 //import Footer from "./Footer.jsx";
 //import Demons from "./Demons.jsx";
 import Card from "./Card.jsx";
+import Counter from "./Counter.jsx";
 function App() {
   
   return(
     <>
-    <Card image="Heated_Heart"type="Spell"></Card>
-    <Card image="Emergency_Call"type="Spell"></Card>
-    <Card image ="Righteous_Justice"type="Spell"></Card>
-    <Card image ="Oversoul"type="Spell"></Card>
-    <Card image ="Hero_Flash" type="Spell"></Card>
-    <Card image ="Hero_Signal" type="Trap"></Card>
+    <Counter></Counter>
     </>
   );
 }
 
 export default App
+
+    //<Card image="Heated_Heart"type="Spell"></Card>
+    //<Card image="Emergency_Call"type="Spell"></Card>
+    //<Card image ="Righteous_Justice"type="Spell"></Card>
+    //<Card image ="Oversoul"type="Spell"></Card>
+    //<Card image ="Hero_Flash" type="Spell"></Card>
+    //<Card image ="Hero_Signal" type="Trap"></Card>
