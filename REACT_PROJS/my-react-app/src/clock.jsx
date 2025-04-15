9ef0af9a6f5a0679f149062fb5d39bba705693de
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-04-15 16:26:10 +0100

    clock finale w REACT

--- REACT_PROJS/my-react-app/src/clock.jsx
+++ REACT_PROJS/my-react-app/src/clock.jsx
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from 'react';
 function Clock(){
+
+    const [time, setTime] = useState(new Date()); //set time state + assign date object
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);  //useEffect to update time every 1000 milliseconds
+    //function formatTime(time){
+        //let hours = time.getHours()
+       // let minutes = time.getMinutes()
+        //let seconds = time.getSeconds()
+       // let meriduim = hours >= 12 ? 'PM' : 'AM'
+    //}
+    function padZero(num){
+        return num < 10 ? `0${num}` : num;
+    } //function to add leading zero to single digit numbers
+    //function to format time(which contains the date object) in 12 hour format
+    const formattedTime = `${padZero(time.getHours())}:${padZero(time.getMinutes())}:${padZero(time.getSeconds())} ${time.getHours() >= 12 ? 'PM' : 'AM'}`;
     return(
         <>
         <div classname="clock-container">
             <div className="clock">
-                <span className="clock-text">00:00:00</span>
-                
+                <span className="clock-text">{formattedTime}</span>
             </div>
         </div>
         </>
