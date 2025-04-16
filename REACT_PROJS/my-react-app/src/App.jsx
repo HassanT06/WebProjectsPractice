88de87667243e84991abe9895544c1a0ecfbfd8e
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-04-16 16:37:01 +0100

    H.E.R.O

--- REACT_PROJS/my-react-app/src/App.jsx
+++ REACT_PROJS/my-react-app/src/App.jsx
@@ -1,25 +1,24 @@
-//import Header from "./Header.jsx";
-//import Footer from "./Footer.jsx";
-//import Demons from "./Demons.jsx";
-//import Card from "./Card.jsx";
-//import Counter from "./Counter.jsx";
-//import Todolist from "./todolist.jsx";
-//import Clock from "./clock.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Demons from "./Demons.jsx";
+import Card from "./Card.jsx";
+import Counter from "./Counter.jsx";
+import Todolist from "./todolist.jsx";
+import Clock from "./clock.jsx";
 import Stopwatch from "./stopwatch.jsx";
 function App() {
   
   return(
     <>
-      <Stopwatch></Stopwatch>
+    <Card image="Heated_Heart"type="Spell"></Card>
+    <Card image="Emergency_Call"type="Spell"></Card>
+    <Card image ="Righteous_Justice"type="Spell"></Card>
+    <Card image ="Oversoul"type="Spell"></Card>
+    <Card image ="Hero_Flash" type="Spell"></Card>
+    <Card image ="Hero_Signal" type="Trap"></Card>
     </>
   );
 }
 
 export default App
-    //<Counter></Counter>
-    //<Card image="Heated_Heart"type="Spell"></Card>
-    //<Card image="Emergency_Call"type="Spell"></Card>
-    //<Card image ="Righteous_Justice"type="Spell"></Card>
-    //<Card image ="Oversoul"type="Spell"></Card>
-    //<Card image ="Hero_Flash" type="Spell"></Card>
-    //<Card image ="Hero_Signal" type="Trap"></Card>
+    
