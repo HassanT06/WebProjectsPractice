3c1fcc7348d8f6a97c4af809d2bb9b6e6d1776ba
Hassan Tanveer <CSMMTANV@ljmu.ac.uk>
2025-05-12 17:09:33 +0100

    Delete Javascript-Html-Css/CSS/HTML-CSS-JS-PROJS directory

--- Javascript-Html-Css/CSS/HTML-CSS-JS-PROJS/RandomNumberGenerator/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("b1").onclick = function(){
-
-    rng1 = Math.floor((Math.random()*6)+1)
-    document.getElementById("l1").textContent = rng1
-    rng2 = Math.floor((Math.random()*6)+1)
-    document.getElementById("l2").textContent = rng2
-    rng3 = Math.floor((Math.random()*6)+1)
-    document.getElementById("l3").textContent = rng3
-}
